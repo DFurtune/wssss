dbcea4daa93ace1f34af88acaa0e812aa15fc2f4
DFurtune <d.furtune18@gmail.com>
2022-09-18 14:01:45 +0300

    first commit

---
+++ build/js/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(document).ready(function () {
+  $(".multiple-items").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    nextArrow: '<button type="button" class="slick-next"></button>',
+    prevArrow: '<button type="button" class="slick-prev"></button>'
+  });
+});
+//# sourceMappingURL=app.js.map
