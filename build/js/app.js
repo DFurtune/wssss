e34b0e5ebfe5bbb71f35b1af600dfe91cf6d754d
DFurtune <d.furtune18@gmail.com>
2022-09-20 11:09:26 +0300

    fix

--- build/js/app.js
+++ build/js/app.js
@@ -10,5 +10,22 @@ $(document).ready(function () {
     nextArrow: '<button type="button" class="slick-next"></button>',
     prevArrow: '<button type="button" class="slick-prev"></button>'
   });
+
+  function updatemenu() {
+    if (document.getElementById("responsive-menu").checked == true) {
+      document.getElementById("menu").style.borderBottomRightRadius = "0";
+      document.getElementById("menu").style.borderBottomLeftRadius = "0";
+    } else {
+      document.getElementById("menu").style.borderRadius = "10px";
+    }
+  }
+
+  $(".single-item").slick({
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  });
 });
 //# sourceMappingURL=app.js.map
